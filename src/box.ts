447e33894355ceb4538afee815fd93d1b16f8e96
shayan davodi <davodi.shayan@gmail.com>
2020-01-06 20:03:17 +0330

    event

--- src/box.ts
+++ src/box.ts
@@ -1,19 +1,25 @@
 import * as moment from 'moment-jalaali';
 export class Box {
-    _boxDate;
-    constructor(offset: number) {
-        let date = moment().add(offset, 'days');
-        this._boxDate = date.format('jYYYY/jM/jD');
+  _boxDate;
+  _fullDate;
+  constructor(offset: number) {
+    let date = moment().add(offset, 'days');
+    this._fullDate= date.format('jYYYY/jM/jD')
+    this._boxDate = date.format('jD');
+  }
+  create(color = '') {
+    let text = '';
+    if (color) {
+      text = 'text-white';
     }
-    create(color = '') {
-        return`
-        <div class="card ${color}">
-        <div class="card-body">
-          <h5 class="card-title">${this._boxDate}</h5>
-          <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+    return `
+        <div class="calender-card card ${color} ${text}" date='${this._fullDate}'>
+        <div class="card-body custom-card">
+          <h5 class="card-title" style='text-align:right'>${this._boxDate}</h5>
+          <h6 class="card-subtitle mb-2"></h6>
         </div>
       </div>
         `
-    }
+  }
 
 }
--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,15 @@ declare const $: any;
 module.exports = {
   create: function (id) {
     $("#" + id).append(calender.style());
+    setTimeout(() => {
+      $(".calender-card").on('click',(e)=>{
+        // console.log(e.target);
+        let elem = $(e.target).closest('.calender-card');
+        let date = $(elem).attr('date');
+        elem['date'] = date;
+        $(elem).trigger('boxClick',[date]);
+      })
+    }, 0);
+
   },
-  clickinDay: function (e) {
-    console.log(e);
-  }
 };
